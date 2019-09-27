4b57e2b02a26800dea435c9124ae8e07b0d9b1c8
Yuranuch <yura477573@mail.ru>
2019-09-27 23:53:08 +0300

    add stopPoint

--- src/Components/Dialogs/Dialogs.js
+++ src/Components/Dialogs/Dialogs.js
@@ -4,8 +4,31 @@ import styles from './Dialogs.module.css'
 
 const Dialogs =(props)=> {
     return (
-        <div>
-            Dialogs
+        <div className={styles.dialogs}>
+           <div className={styles.dialogs_items}>
+               <div className={styles.dialog}>
+                    Yura
+               </div>
+               <div className={styles.dialog}>
+                    Natasha
+               </div>
+               <div className={styles.dialog}>
+                   Genya
+               </div>
+
+           </div>
+            <div className={styles.messages}>
+                <div className={styles.message}>
+                    Hello
+                </div>
+                <div className={styles.message}>
+                    Hello haw are you&
+                </div>
+                <div className={styles.message}>
+                    I am fine
+                </div>
+
+            </div>
         </div>
     );
 }
