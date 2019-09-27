75d51ecb99ee1be83aa3d49310525623d5842570
Yuranuch <yura477573@mail.ru>
2019-09-27 21:59:49 +0300

    first commit

--- src/Components/Profile/MyPosts/Post/Post.js
+++ src/Components/Profile/MyPosts/Post/Post.js
@@ -6,6 +6,7 @@ const Post = () => {
     return (
 
         <div className={styles.post}>
+
             <img src="http://gloria-mur.ru/wp-content/uploads/2017/05/avatar1-740x463.jpg"/>
             Post 2
         </div>
