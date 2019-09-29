ca76f0f5ea40183a4820cab4be08fbfcc729b7aa
Yuranuch <yura477573@mail.ru>
2019-09-29 18:17:47 +0300

    ready for install axios

--- src/Components/Users/Users.js
+++ src/Components/Users/Users.js
@@ -2,11 +2,8 @@ import React from "react"
 import styles from './Users.module.css'
 const Users =(props) => {
     if(props.users.length===0) {
-    props.setUsers([
-        {id: 0, photo: "https://klike.net/uploads/posts/2019-03/1551511801_1.jpg", userName: "Yura", follow: true, status: "I'm a boss", location: {city: "Minsk"}},
-        {id: 1, photo: "https://klike.net/uploads/posts/2019-03/1551511801_1.jpg", userName: "Sasha", follow: false, status: "I'm a boss too", location: {city: "Brest"}},
-        {id: 2, photo: "https://klike.net/uploads/posts/2019-03/1551511801_1.jpg", userName: "Andrey", follow: false, status: "I'm a boss", location: {city: "Vitebsk"}},
-        ]
+    props.setUsers(
+
 
     )}
     let onFollowMe = (userId) => {
