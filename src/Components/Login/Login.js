c0bf6b9e660badd043be4b888f472bcaf26e5fba
Yuranuch <yura477573@mail.ru>
2019-10-07 17:15:08 +0300

    added LoginForm

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -2,8 +2,29 @@ import React from "react"
 const Login = () => {
     return (
         <div>
-            Login
+            <h1>Login</h1>
+            <LoginForm/>
         </div>
     )
 }
+
+const LoginForm = () => {
+    return (
+        <form>
+            <div>
+                <input placeholder={"Login"}/>
+            </div>
+            <div>
+                <input placeholder={"Password"} />
+            </div>
+            <div>
+                <input type={"checkbox"}/> Remember Me
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
+    )
+}
+
 export default Login
