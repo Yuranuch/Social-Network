c2044c5895525c1c3ea8cebf935642b2cf77acb1
Yuranuch <yura477573@mail.ru>
2019-10-07 17:01:59 +0300

    all ready to add loginForm

--- src/Components/Profile/MyPosts/MyPostsContainer.js
+++ src/Components/Profile/MyPosts/MyPostsContainer.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import {connect} from "react-redux";
 import {addNewPost, changePostText} from "../../../redux/profile-reducer";
 import MyPosts from "./MyPosts";
--- src/Components/Users/Users.js
+++ src/Components/Users/Users.js
@@ -1,9 +1,6 @@
 import React from "react"
 import styles from "./Users.module.css";
 import {NavLink} from "react-router-dom";
-import * as axios from "axios";
-import {followMe, UnfollowMe, usersAPI} from "../../API/api";
-
 
 const Users =(props)=> {
 
